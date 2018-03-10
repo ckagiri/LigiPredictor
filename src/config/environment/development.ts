6fc9bafedd8bcf020a057aa3c3dc48b5e272fda8
ckagiri <charleskagiri@hotmail.com>
2018-03-10 14:51:46 +0300

    removed environment files

--- src/config/environment/development.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    env: 'development',
-    port: process.env.PORT || 3000,
-    mongo: {
-      uri: 'mongodb://localhost:27017/test123-dev'
-    }
-  }
--- src/config/environment/production.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    env: 'production'
-};
-//# sourceMappingURL=production.js.map
