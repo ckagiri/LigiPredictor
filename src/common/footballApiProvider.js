39a4eb1b90ea107592cac077f73c79ebcec4a2ea
ckagiri <charleskagiri@hotmail.com>
2018-04-11 16:13:09 +0300

    rearrangement: make room for angular

--- src/common/footballApiProvider.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var FootballApiProvider;
-(function (FootballApiProvider) {
-    FootballApiProvider["LIGI"] = "LIGI";
-    FootballApiProvider["API_FOOTBALL_DATA"] = "API_FOOTBALL_DATA";
-})(FootballApiProvider = exports.FootballApiProvider || (exports.FootballApiProvider = {}));
-//# sourceMappingURL=footballApiProvider.js.map
--- src/db/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const league_model_1 = require("./league.model");
-exports.League = league_model_1.LeagueModel;
-exports.default = [
-    exports.League
-];
-//# sourceMappingURL=index.js.map
